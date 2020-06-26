90310d57d26e26c18becced18b12072c1eb21da6
Leon Suckow <leonlucassuckow@hotmail.com>
2020-06-26 15:53:38 -0300

    Desafio semana 3 - fundamentos node_concluido

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,9 +1,19 @@
 import Transaction from '../models/Transaction';
-
 interface Balance {
   income: number;
   outcome: number;
   total: number;
+}
+
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
+interface ListTransactionDTO {
+  transactions: Transaction[],
+  balance: Balance
 }
 
 class TransactionsRepository {
@@ -13,16 +23,37 @@ class TransactionsRepository {
     this.transactions = [];
   }
 
-  public all(): Transaction[] {
-    // TODO
+  public all(): ListTransactionDTO {
+    const balance = this.getBalance();
+
+    const transactions: ListTransactionDTO = {
+      transactions: this.transactions,
+      balance
+    }
+    return transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const totalIncome = this.transactions.reduce((totalIncome, transaction) => {
+      return transaction.type === "income" ? totalIncome + transaction.value : totalIncome
+    }, 0);
+
+    const totalOutcome = this.transactions.reduce((totalOutcome, transaction) => {
+      return transaction.type === "outcome" ? totalOutcome + transaction.value : totalOutcome
+    }, 0);
+
+    const balance = {
+      income: totalIncome,
+      outcome: totalOutcome,
+      total: totalIncome - totalOutcome
+    };
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+    this.transactions.push(transaction);
+    return transaction;
   }
 }
 
